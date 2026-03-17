1a4aa73925c3e672bb25d12c148401bf2d0b2388
dwesh163 <dwesh163@protonmail.com>
2026-03-17 08:06:31 +0100

    [feature] implement authentication with Microsoft Entra ID and user session management

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,44 @@
+import type { DefaultSession } from "next-auth";
+import type { DefaultJWT } from "next-auth/jwt";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      name: string;
+      email: string;
+      image?: string;
+      userId: string;
+      username: string;
+      oid: string;
+      tid: string;
+      groups: string[];
+    } & DefaultSession["user"];
+    expires: string;
+  }
+
+  interface UserInfo {
+    sub: string;
+    accreds: string[];
+    cfs: string[];
+    groups: string[];
+    name: string;
+    rights: string[];
+    roles: string[];
+    uniqueid: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT extends DefaultJWT {
+    access_token: string;
+    expires_at: number;
+    oid: string;
+    tid: string;
+    name: string;
+    email: string;
+    uniqueId: string;
+    username: string;
+    groups: string[];
+    error?: string;
+  }
+}
